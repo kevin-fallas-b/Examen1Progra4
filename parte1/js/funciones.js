acb4e5f79c52afe2ffdcb81a9258d57f826c16b2
Kevin Fallas <kevin.fallas.b@gmail.com>
2020-04-19 23:21:39 -0600

    ventana se mueve pero tiene error que detiene el js

--- parte1/js/funciones.js
+++ parte1/js/funciones.js
@@ -1,7 +1,20 @@
 window.addEventListener('load',funcionInicial, false);
 
+var ventanas;
+var relojsup;
+
 function funcionInicial(){
+
+    //encontrar app y darle evento de abrir
+    var apps = document.getElementsByClassName("icons");
+    for(var i=0; i<apps.length;i++){
+        apps[i].addEventListener("click",openApp,false);
+    }
+
+    //click sobre el reloj abre calendario
+    relojsup = document.getElementById("relojsuperior");
     setInterval("reloj()",1000);
+    relojsup.addEventListener('click',abrircalendario,false);
 }
 
 function reloj(){
@@ -11,5 +24,56 @@ function reloj(){
     var hora = fecha.getHours();
     var minutos = fecha.getMinutes();
     var segundos = fecha.getSeconds();
-    document.getElementById("relojsuperior").innerHTML = semana[dia] + " "+ hora+ ":"+ minutos+ ":" + segundos;
+    relojsup.innerHTML = semana[dia] + " "+ hora+ ":"+ minutos+ ":" + segundos;
+}
+
+function openApp(){
+    document.getElementById("pantalla").innerHTML +="<div class='contenedorapp'>"+
+                                                        " <img src='./rsc/img/exit.png' class='botonsalir'>"+
+                                                        " <label class='nombreapp'>"+this.getAttribute('data-value')+" </label>"+
+                                                        " <object class='app' type='text/html' data='./rsc/apps/"+this.id+"/index.html' width='500px' height='300px'>"+
+                                                        " </object>"+
+                                                    "</div>";
+
+    ventanas = document.getElementsByClassName("contenedorapp");
+    agregarDragAndDrop(ventanas.length-1);
+    agregarsalirapp();
+}
+
+function agregarDragAndDrop(){
+    ventanas[ventanas.length-1].addEventListener('mousedown',mousedown);
+}
+
+function mousedown(e){
+    window.addEventListener('mousemove',mousemove);
+    window.addEventListener('mouseup',mouseup);
+
+    var prevX = e.clientX;
+    var prevY = e.clientY;
+
+    function mousemove(e){
+        var newX = prevX - e.clientX;
+        var newY = prevY - e.clientY;
+
+        var rect = ventanas[ventanas.length-1].getBoundingClientRect();
+
+        ventanas[ventanas.length-1].style.left = rect.left - newX + "px";
+        ventanas[ventanas.length-1].style.top = rect.top - newY + "px"
+
+        prevX = e.clientX;
+        prevY = e.clientY;
+    }
+
+    function mouseup(){
+        window.removeEventListener('mousemove',mousemove);
+        window.removeEventListener('mouseup',mouseup);
+    }
+}
+
+function agregarsalirapp(){
+ 
+}
+
+function abrircalendario(){
+    console.log("abrio calendario");
 }
